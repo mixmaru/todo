3d205bfeba5ebedb4b3a56ac23317673d3240902
mix <mix.touch777@gmail.com>
2016-06-19 16:17:49 +0900

    リクエストクラスで、post値、get値を返すメソッドの実装を行った

--- classes/Request.php
+++ classes/Request.php
@@ -35,16 +35,59 @@ class Request
     /**
      * @param null $key
      * @return string
-     * get値またはpost値から指定のkeyのデータを取得する。
+     * get値から指定のkeyのデータを取得する。
      * nullが渡された場合は、全ての値を配列にもたせて返す
      * 値がなければfalseを返す
      */
     public function get($key = null){
-
-        return "値";
+        return $this->getValue("get", $key);
     }
 
+    /**
+     * @param null $key
+     * @return string
+     * post値から指定のkeyのデータを取得する。
+     * nullが渡された場合は、全ての値を配列にもたせて返す
+     * 値がなければfalseを返す
+     */
     public function post($key = null){
-        return "値";
+        return $this->getValue("post", $key);
+    }
+
+    /**
+     * getかpostから指定keyの値を取得する。keyがなければfalseを返す
+     * keyが指定されていなければ全ての値を返す
+     * @param $method "get" or "post"
+     * @param null $key
+     * @return mixed
+     * @throws \Exception
+     */
+    private function getValue($method, $key = null){
+        if($method != "get" && $method != "post"){
+            throw new \Exception("getかpostを指定してください");
+        }
+
+        //$_GETや$_POSTは可変変数で扱うことができず、get,postの文字列をつかって動的に分けることが出来なかった。
+        //とりあえずしょうがなくcaseで分岐させる
+        switch($method){
+            case "get":
+                $values = $_GET;
+                break;
+            case "post":
+                $values = $_POST;
+                break;
+            default:
+                throw new \Exception("不明なエラーが発生しました");
+        }
+
+        $ret_value = false;
+        if(is_null($key)){
+            //全ての値を返す
+            $ret_value = $values;
+        }else{
+            //$keyの値が存在するならそれを返す。
+            $ret_value = (isset($values[$key])) ? $values[$key] : false;
+        }
+        return $ret_value;
     }
 }
