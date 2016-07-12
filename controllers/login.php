e04a370949be89e92862ba6da03d654257f52092
mix <mix.touch777@gmail.com>
2016-07-12 21:45:23 +0900

    ログインとログインチェックの擬似コード書いた

---
+++ controllers/login.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mix
+ * Date: 2016/07/11
+ * Time: 23:41
+ *
+ * ログインプロセスの擬似コード
+ */
+
+function login($id, $pass){
+    /*todo:
+    //google Oauthを使って認証。user_idを取得
+    if(認証ok){
+        [ログイン済]へuser_idを保存
+        return true;
+    }else{
+        return false;
+    }
+    */
+}
---
+++ controllers/loginCheck.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mix
+ * Date: 2016/07/11
+ * Time: 23:58
+ *
+ * ログインチェックプロセスの擬似コード
+ */
+
+function loginCheck(){
+    /*todo:
+    if([ログイン済]にuser_idがある){
+        return true;
+    }else{
+        return false;
+    }
+    */
+}
