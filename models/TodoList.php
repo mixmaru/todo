7bc4221a8d2f516bced9478c7b5f3a3ef6341a6d
mix <mix.touch777@gmail.com>
2016-07-19 23:21:18 +0900

    テストの残骸

--- models/TodoList.php
+++ models/TodoList.php
@@ -12,31 +12,66 @@ namespace models;
 
 use models\Todo;
 
-class TodoList
+class TodoList extends BaseModel
 {
-    private $models;
+    private $todoList;
 
-    /**
-     * todoList constructor.
-     */
-    public function __construct()
-    {
-        $this->models = [new todo()];
+    static public function getTodoListByProject($project_id){
+        $ret_obj = new TodoList();
+        $ret_obj->todoList = [];
+
+        $sql = "SELECT * FROM ".Todo::TABLE_NAME." WHERE project_id = :project_id ORDER BY depth ASC";
+        $params = [':project_id' => $project_id];
+
+        $stmt = self::$pdo->prepare($sql);
+        if($stmt->execute($params)){
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            if($result !== false){
+                $tmp_list = [];
+                $tmp_tree = [];
+                foreach($result as $data){
+                    $todo_obj = new Todo();
+                    $todo_obj->setId((int) $data['id']);
+                    $todo_obj->setTitle($data['title']);
+                    $todo_obj->setDoDate($data['do_date']);
+                    $todo_obj->setLimitData($data['limit_date']);
+                    $todo_obj->setIsDone($data['is_done']);
+                    $todo_obj->setPath($data['path']);
+                    $todo_obj->setProjectId($data['project_id']);
+                    $todo_obj->setUserId($data['user_id']);
+//                    $todo_obj->created      = $data['created'];
+//                    $todo_obj->modified     = $data['modified'];
+
+                    $path_array = explode("/", $todo_obj->getPath());
+                    array_pop($path_array);//から要素の除去
+                    array_shift($path_array);//から要素の除去
+                    $current_id = array_pop($path_array);
+                    $parent_id = array_pop($path_array);//なければnull
+                    $tmp_list[$current_id] = [
+                        'obj' => $todo_obj,
+                        'child' => [],
+                    ];
+                    if(!isset($parent_id)){
+                        $tmp_tree[$current_id] = &$tmp_list[$current_id];
+                    }else{
+                        $tmp_list[$parent_id]['child'][$current_id] = &$tmp_list[$current_id];
+                    }
+                }
+                $ret_obj->todoList = $tmp_tree;
+            }
+        }
+        return $ret_obj;
     }
 
     public function getDataArray(){
-        $todo_data_array = [];
-        for($i=0; $i<10; $i++){
-            $todo_data_array[] = [
-                'id' => $i,
-                'done' => false,
-                'title' => "タイトル",
-                'limit_date' => "2016-05-30",
-                'view_order' => 30,
-                'create_date' => "2016-05-0$i 21:11:11",
-                'last_update_date' => "2016-05-0$i 21:11:11"
-            ];
+        foreach($this->todoList as $todo){
+            var_dump($todo['obj']->getId());
+            foreach($todo['child'] as $todo2){
+                var_dump($todo2['obj']->getId());
+                foreach($todo2['child'] as $todo3){
+                    var_dump($todo3['obj']->getId());
+                }
+            }
         }
-        return $todo_data_array;
     }
 }
