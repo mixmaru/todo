3b6f08c4eea62cd7e58e877cd2254220850809b4
mix <mix.touch777@gmail.com>
2016-08-11 15:52:20 +0900

    ユニットテストしようとしたがDBを使ったunitTestの設定の仕方がいまいちわからず。書籍かcakePHPなどフレームワークでテストしてみて勉強してから行うことにした

---
+++ test/GenericTestsDatabaseTestCase.php
@@ -0,0 +1,31 @@
+<?php
+namespace test;
+/**
+ * Created by PhpStorm.
+ * User: mix
+ * Date: 2016/08/11
+ * Time: 12:53
+ */
+abstract class GenericTestsDatabaseTestCase extends \PHPUnit_Extensions_Database_TestCase
+{
+    // PDO のインスタンス生成は、クリーンアップおよびフィクスチャ読み込みのときに一度だけ
+    static private $pdo = null;
+
+    // PHPUnit_Extensions_Database_DB_IDatabaseConnection のインスタンス生成は、テストごとに一度だけ
+    private $conn = null;
+
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new \PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        }
+
+        return $this->conn;
+    }
+}
--- test/TodoTest.php
+++ test/TodoTest.php
@@ -10,34 +10,69 @@ namespace test;
 
 use PHPUnit\Framework\TestCase;
 use models\Todo;
+use test\GenericTestsDatabaseTestCase;
 
 //初期化ファイル読み込み
 define("ENV", "unit_test");
 include dirname(__FILE__)."/../init/init.php";
 
-class TodoTest extends TestCase{
+//class TodoTest extends \PHPUnit_Extensions_Database_TestCase{
+class TodoTest extends GenericTestsDatabaseTestCase{
+
+//    public function getConnection()
+//    {
+//        var_dump("move");
+//        $pdo = new \PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+//        return $this->createDefaultDBConnection($pdo, ':memory:');
+//    }
+
+//    public function getDataSet()
+//    {
+//        var_dump("データセットされた");
+//        return $this->createMySQLXMLDataSet('test/db_test_data.xml');
+//    }
+    /**
+     * @return \PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet('test/db_test_data.xml');
+//        return new \PHPUnit_Extensions_Database_DataSet_YamlDataSet(
+//            dirname(__FILE__)."/_files/guestbook.yml"
+//        );
+    }
+
 
     public function setUp(){
         //初期化
+        var_dump("初期化された");
     }
 
     public function tearDown(){
     }
 
-    public function testDataCheck(){
-        var_dump(Todo::getTodoListByUser(1));
-//        $this->assertEquals("", Todo::getTodoListByUser(1));
+    public function testGetRowCount()
+    {
+        $this->assertEquals(2, $this->getConnection()->getRowCount('db_test_data'));
     }
 
-    public function testPushAndPop(){
-//        $stack = [];
-//        $this->assertEquals(0, count($stack));
+//    public function testDataCheck(){
+//        var_dump("test実行された");
+//        $this->getConnection()->createDataSet();
+//        $this->getDataSet();
+////        var_dump(Todo::getTodoListByUser(1));
+////        $this->assertEquals("", Todo::getTodoListByUser(1));
+//    }
 //
-//        array_push($stack, 'foo');
-//        $this->assertEquals('foo', $stack[count($stack)-1]);
-//        $this->assertEquals(1, count($stack));
-//
-//        $this->assertEquals('foo', array_pop($stack));
-//        $this->assertEquals(0, count($stack));
-    }
+//    public function testPushAndPop(){
+////        $stack = [];
+////        $this->assertEquals(0, count($stack));
+////
+////        array_push($stack, 'foo');
+////        $this->assertEquals('foo', $stack[count($stack)-1]);
+////        $this->assertEquals(1, count($stack));
+////
+////        $this->assertEquals('foo', array_pop($stack));
+////        $this->assertEquals(0, count($stack));
+//    }
 }
