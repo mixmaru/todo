0edbcb90be559f91de202dfbeca13ab9bba74155
mix <mix.touch777@gmail.com>
2016-08-13 21:48:49 +0900

    gulp.spritesmithをインストールして、cssスプライト画像作成(retina対応)を行った

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,39 @@
 var gulp = require('gulp');
 var sass = require("gulp-ruby-sass");
 var sftp = require("gulp-sftp");
+var spritesmith = require("gulp.spritesmith");
 
 gulp.task('default', function() {
     gulp.watch("./sass/**/*.scss", ["sass"]);
     gulp.watch("./www/css/**/*.css", ["css-upload"]);
 });
 
+//scss -> css変換
 gulp.task('sass', function() {
     return sass('./sass/**/*.scss', { compass: true })
         .on('error', sass.logError)
         .pipe(gulp.dest('./www/css'));
 });
 
+//cssスプライト画像と対応scss作成
+//https://www.npmjs.com/package/gulp.spritesmith
+//http://blog.e-riverstyle.com/2014/02/gulpspritesmithcss-spritegulp.html
+gulp.task('sprite', function(){
+    var spriteData = gulp.src('./images/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_sprite.scss',
+            imgPath: '/img/sprite.png',
+            cssFormat: 'scss',
+            retinaSrcFilter: './images/*-2x.png',
+            retinaImgName: 'sprite-2x.png',
+            retinaImgPath: 'images/sprite-2x.png'
+        }));
+    spriteData.img.pipe(gulp.dest('./www/img/'));
+    spriteData.css.pipe(gulp.dest('./sass/'));
+});
+
+//cssのアップロード
 gulp.task('css-upload', function(){
     var host_conf = require("./config/host.json");
     return gulp.src('www/css/**/*.css')
