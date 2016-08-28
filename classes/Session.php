084133d4c0a1abc8915904ad48f46687432c03be
mix <mix.touch777@gmail.com>
2016-08-28 16:56:18 +0900

    session_startが複数回実行されないようにした。sessionの値の削除メソッドを追加した

--- classes/Session.php
+++ classes/Session.php
@@ -15,12 +15,17 @@ class Session
 
     public function __construct()
     {
-        session_start();//todo:セッション周り調べる
+        if(session_status() == PHP_SESSION_NONE){
+            session_start();//todo:セッション周り調べる
+        }
     }
     public function set($key, $value){
         $_SESSION[$key] = $value;
     }
     public function get($key){
         return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+    public function delete($key){
+        unset($_SESSION[$key]);
     }
 }
